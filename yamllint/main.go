331a5d84a64890c071dddb41b74326f8aeece5b4
Thomas Hamm <th.hm@proton.me>
2024-08-31 10:16:40 +0200

    Support linting of individual files and strict mode

--- yamllint/main.go
+++ yamllint/main.go
@@ -10,9 +10,14 @@ package main
 import (
 	"context"
 	"dagger/yamllint/internal/dagger"
+	"errors"
+	"fmt"
+	"path/filepath"
 )
 
 const YAMLLINT_VERSION = "1.35.1-r1"
+const DEFAULT_PATH = "."
+const program = "yamllint"
 
 type Yamllint struct{}
 
@@ -22,17 +27,55 @@ func (m *Yamllint) Lint(
 	src *dagger.Directory,
 	// +optional
 	version string,
+	// +optional
+	path string,
+	// +optional
+	strict bool,
 ) (string, error) {
 	if version == "" {
 		version = YAMLLINT_VERSION
 	}
 
-	return dag.Container().
+	if path == "" {
+		path = DEFAULT_PATH
+	} else {
+		path = filepath.Clean(path)
+	}
+
+	c := dag.Container().
 		From("alpine:latest").
 		WithExec([]string{"apk", "update"}).
 		WithExec([]string{"apk", "add", "yamllint=" + version}).
 		WithMountedDirectory("/mnt", src).
 		WithWorkdir("/mnt").
-		WithExec([]string{"yamllint", "."}).
-		Stdout(ctx)
+		WithExec(
+			command(
+				program, strict, path))
+
+	stdout, err := c.Stdout(ctx)
+
+	if err != nil {
+		var e *dagger.ExecError
+		if errors.As(err, &e) {
+			return stdout, fmt.Errorf("yamllint failed with exit code %d", e.ExitCode)
+		}
+
+		return stdout, err
+	}
+
+	return stdout, nil
+}
+
+func command(exec string, strict bool, path string) []string {
+	var args []string
+
+	args = append(args, exec)
+
+	if strict {
+		args = append(args, "--strict")
+	}
+
+	args = append(args, path)
+
+	return args
 }
